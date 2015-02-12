aafe76bd32224225ec9c8ea6b07f4090c889789d
LarryZ <zhangyang1009@gmail.com>
2015-02-12 17:50:32 +0800

    final version remove

--- Run.py
+++ Run.py
@@ -1,6 +1,7 @@
 import TaggingSystem
-import tag
-import item
+from tag import tag,item
 
 if __name__=='__main__':
+    import sys
+    print sys.path
     TaggingSystem.app.run(debug=True,host='0.0.0.0')
