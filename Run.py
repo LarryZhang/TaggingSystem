fbe14996f5201cdcd5eeb8d5cd8f39cc17c5e4ed
LarryZ <zhangyang1009@gmail.com>
2015-02-12 17:31:59 +0800

    final version remove

--- Run.py
+++ Run.py
@@ -1,5 +1,6 @@
 import TaggingSystem
-
+import tag
+import item
 
 if __name__=='__main__':
     TaggingSystem.app.run(debug=True)
--- TaggingSystem.py
+++ TaggingSystem.py
@@ -27,4 +27,4 @@ api.representations = DEFAULT_REPRESENTATIONS
 
 print __name__
 if __name__=='__main__':
-    app.run(debug=True)
+    app.run(debug=True,host='0.0.0.0')
